a86e9674be7b6ab1d17ecf874ce908f4c4b09c7a
Víctor Alfonso Mejía Morales <130195315+vamm99@users.noreply.github.com>
2025-03-18 23:50:28 -0500

    feat: creación de migraciones y modelo notificación

---
+++ app/Models/Notificacion.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notificacion extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'usuario_id',
+        'mensaje',
+        'leida'
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class);
+    }
+}
