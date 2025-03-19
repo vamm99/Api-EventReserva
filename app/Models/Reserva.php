6663f09ef14c53e9614ab7ce2332d2e7c14bd7e1
Víctor Alfonso Mejía Morales <130195315+vamm99@users.noreply.github.com>
2025-03-18 23:49:00 -0500

    feat: creación de migraciones y modelo de reserva

---
+++ app/Models/Reserva.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reserva extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'usuario_id',
+        'evento_id',
+        'estado'
+    ];
+
+    public function usuario()
+    {
+        return $this->belongsTo(Usuario::class);
+    }
+
+    public function evento()
+    {
+        return $this->belongsTo(Evento::class);
+    }
+}
