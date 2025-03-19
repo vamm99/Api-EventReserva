3ffcc8300f21ea043a954a4683195001a33a18a1
Víctor Alfonso Mejía Morales <130195315+vamm99@users.noreply.github.com>
2025-03-18 23:45:06 -0500

    Feat: creación de migraciones y modelo de usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Usuario extends Model
+class Usuario extends Authenticatable
 {
-    //
+    use HasFactory, Notifiable;
+
+    protected $fillable = ['nombre', 'email', 'password', 'rol'];
+
+    protected $hidden = ['password'];
+
+    protected $casts = [
+        'rol' => 'string',
+    ];
 }
--- database/migrations/2025_03_19_043717_create_usuarios_table.php
+++ database/migrations/2025_03_19_043717_create_usuarios_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('rol', ['admin', 'organizador', 'usuario'])->default('usuario');
             $table->timestamps();
         });
     }
